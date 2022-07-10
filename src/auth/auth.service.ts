e0aa5061afec699f1e74edbf8d5cfc6183dacb59
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-07-10 03:37:14 -0700

    Added, Setup Prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  logIn() {
+    return { res: 'Hello' }
+  }
+  signUp() {
+    return { res: 'Welcome' }
+  }
+}
