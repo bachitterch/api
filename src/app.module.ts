5dc887e188ef7789c7a32b0482c63c53f74a78ff
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-07-10 03:02:44 -0700

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { AuthModule } from './auth/auth.module'
+import { BookmarksModule } from './bookmarks/bookmarks.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { UserModule } from './user/user.module'
+
+@Module({
+  imports: [AuthModule, BookmarksModule, PrismaModule, UserModule],
+})
+export class AppModule {}
